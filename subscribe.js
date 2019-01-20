2d48f7e7e59bda34bcae1675d8cff577acdf1314
eipanoxi <46861949+eipanoxi@users.noreply.github.com>
2019-01-20 18:49:51 +0000

    Add files via upload

---
+++ subscribe.js
@@ -0,0 +1,291 @@
+var countryList = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua &amp; Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia &amp; Herzegovina",
+  "Botswana",
+  "Brazil",
+  "British Virgin Islands",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Cayman Islands",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Cote D Ivoire",
+  "Croatia",
+  "Cruise Ship",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Polynesia",
+  "French West Indies",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kuwait",
+  "Kyrgyz Republic",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Namibia",
+  "Nepal",
+  "Netherlands",
+  "Netherlands Antilles",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Pierre &amp; Miquelon",
+  "Samoa",
+  "San Marino",
+  "Satellite",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "South Africa",
+  "South Korea",
+  "Spain",
+  "Sri Lanka",
+  "St Kitts &amp; Nevis",
+  "St Lucia",
+  "St Vincent",
+  "St. Lucia",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor L'Este",
+  "Togo",
+  "Tonga",
+  "Trinidad &amp; Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks &amp; Caicos",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "United States Minor Outlying Islands",
+  "Uruguay",
+  "Uzbekistan",
+  "Venezuela",
+  "Vietnam",
+  "Virgin Islands (US)",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe"
+];
+
+function getFormData($form) {
+  var unindexed = $form.serializeArray();
+  var indexed = {};
+
+  $.map(unindexed, function(n, i) {
+    indexed[n["name"]] = n["value"];
+  });
+
+  return indexed;
+}
+
+$(document).ready(function() {
+  $("#registerForm").submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/subscribe",
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify(getFormData($("#registerForm"))),
+      success: function(result) {
+        console.log('ok');
+        $("#registration").html("<div class=\"messageResponse translate\" data-args=\"form-"+result.status+"\" />");
+        translate();
+      },
+      error: function(xhr, resp, text) {
+        console.log('fail', text, resp);
+        $("#registration").html("<div class=\"messageResponse translate\" data-args=\"form-error\" />");
+        translate();
+      }
+    });
+  });
+});
+
+var translate = function() {
+  $(".translate").each(function() {
+    var args = [],
+      $this = $(this);
+    if ($this.data("args")) args = $this.data("args").split(",");
+    $this.html($.i18n.apply(null, args));
+  });
+};
+
+$("#switchlocale").on("click", "a", function(e) {
+  e.preventDefault();
+  $.i18n().locale = $(this).data("locale");
+  translate();
+});
+
+$.i18n({
+  locale: window.frameElement.getAttribute("data-lang")
+})
+  .load({
+    en: "i18n/en.json",
+    el: "i18n/el.json"
+  })
+  .done(function() {
+    translate();
+    for (var i = 1; i <= 12; i++) {
+      $("#monthOfBirth").append(
+        $("<option />")
+          .val(i)
+          .html(i)
+      );
+    }
+  });
+
+for (i = new Date().getFullYear(); i > 1919; i--) {
+  $("#yearOfBirth").append(
+    $("<option />")
+      .val(i)
+      .html(i)
+  );
+}
+
+for (var i = 0; i < countryList.length; i++) {
+  $("#birthPlace").append(
+    $("<option />")
+      .val(countryList[i])
+      .html(countryList[i])
+  );
+}
