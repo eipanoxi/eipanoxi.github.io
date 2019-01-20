bbaa32c31938d0d0551eb751ad3ac6ce95a18618
eipanoxi <46861949+eipanoxi@users.noreply.github.com>
2019-01-20 19:35:06 +0000

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -257,7 +257,7 @@ $("#switchlocale").on("click", "a", function(e) {
 });
 
 $.i18n({
-  locale: window.frameElement.getAttribute("data-lang")
+  locale: 'en' // window.frameElement.getAttribute("data-lang")
 })
   .load({
     en: "https://eipanoxi.github.io/en.json",
